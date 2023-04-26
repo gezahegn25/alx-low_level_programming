ec28b6a63abd93dec051ba70ade638dc02b68f4a
gezahegn25 <gezahegns988@gmail.com>
2023-04-26 09:34:36 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+ /** 
+  * _abs - computes the absolute value of an integer
+  * @n: check
+  * Return: at n or -n
+  */
+int _abs(int)
+
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
